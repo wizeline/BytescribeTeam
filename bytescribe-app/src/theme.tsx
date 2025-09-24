ef49988ebe8e13ea41396f3d539d3c1eb848b150
An Dien <an.dien@wizeline.com>
2025-09-24 13:14:28 +0700

    update

--- bytescribe-app/src/theme.tsx
+++ bytescribe-app/src/theme.tsx
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    mode: "light",
+    mode: "dark",
   },
   //   typography: {
   //     fontFamily: 'var(--font-roboto)',
--- elevenlabs-poc/src/lambda_function.py
+++ elevenlabs-poc/src/lambda_function.py
@@ -4,6 +4,7 @@ from elevenlabs.client import ElevenLabs
 from elevenlabs.play import save
 import boto3
 from botocore.exceptions import NoCredentialsError
+import glob
 
 load_dotenv()
 
@@ -11,29 +12,42 @@ elevenlabs = ElevenLabs(
   api_key=os.getenv("ELEVENLABS_API_KEY"),
 )
 
-def convert_text_to_speech(text, fileName="output.mp3"):
-    print(f"Start converting text: {text}")
+def convert_text_to_speech(text_segments, fileName="output"):
+    all_audio = []
+    previous_text = None
     try:
-        audio = elevenlabs.text_to_speech.convert(
-            text=text,
-            voice_id="JBFqnCBsd6RMkjVDRZzb",
-            model_id="eleven_multilingual_v2",
-            output_format="mp3_44100_128",
-        )
-        print(f'Audio generated, saving to "/tmp/{fileName}..."')
-        save(audio, f'/tmp/{fileName}')
-        print(f'Audio saved as "/tmp/{fileName}"')
+        for i, segment in enumerate(text_segments):
+            print(f"Start converting text: {segment}")
+            audio = elevenlabs.text_to_speech.convert(
+                text=segment,
+                voice_id="JBFqnCBsd6RMkjVDRZzb",
+                model_id="eleven_multilingual_v2",
+                output_format="mp3_44100_128",
+                previous_text=previous_text,
+                next_text=text_segments[i+1] if i + 1 < len(text_segments) else None
+            )
+            all_audio.append(audio)
+            previous_text = segment
+            # save(audio, f'/tmp/{fileName}-{i}.mp3')
+            # print(f"Audio saved as /tmp/{fileName}-{i}.mp3")
+
+        for j, audio_segment in enumerate(all_audio):
+            save(audio_segment, f'/tmp/{fileName}-{j}.mp3')
+            print(f"Audio saved as /tmp/{fileName}-{j}.mp3")
     except Exception as e:
         print(e)
-        print(f'Error requesting ElevenLabs convert "{text}" to speech.')
+        print(f'Error requesting ElevenLabs convert "{text_segments}" to speech.')
         raise e
 
 def upload_file_to_s3(fileName):
     s3 = boto3.client('s3')
     bucketName = "adien-apac-practice-bucket"
     try:
-        s3.upload_file(f'/tmp/{fileName}', bucketName, fileName)
-        print(f'File /tmp/{fileName} uploaded to {bucketName}/{fileName}')
+        audio_files = glob.glob(f"/tmp/{fileName}*.mp3")
+        for audioFileName in audio_files:
+            key = f"audio/{audioFileName}"
+            s3.upload_file(f'/tmp/{audioFileName}', bucketName, key)
+            print(f"File /tmp/{audioFileName} uploaded to {bucketName}/{key}")
     except FileNotFoundError:
         print("The file was not found")
     except NoCredentialsError:
@@ -42,8 +56,8 @@ def upload_file_to_s3(fileName):
         print(f"Error uploading file: {e}")
 
 def lambda_handler(event, context):
-    text = event.get("text", "Hello from Lambda!")
-    output_filename = event.get("output", "output.mp3")
+    text = event.get("text", ["Hello from Lambda!"])
+    output_filename = event.get("output", "output")
 
     convert_text_to_speech(text, output_filename)
     upload_file_to_s3(output_filename)
